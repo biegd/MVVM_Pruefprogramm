983f669aa67b8cacd44b4697043fb20e2136fcfe
biegd <155267165+biegd@users.noreply.github.com>
2025-05-25 20:14:18 +0200

    Add files via upload

---
+++ MVVM_Pruefprogramm/Models/TestStep.cs
@@ -0,0 +1,35 @@
+﻿namespace MVVM_Pruefprogramm.Models;
+
+public abstract class TestStep
+{
+    public string Name { get; }
+
+    protected TestStep(string name)
+    {
+        Name = name;
+    }
+
+    public abstract string Execute();
+}
+
+public class MeasureVoltageStep : TestStep
+{
+    public MeasureVoltageStep() : base("Measure Voltage") { }
+
+    public override string Execute()
+    {
+        double simulatedVoltage = 3.0 + new Random().NextDouble() * 2.0;
+        return $"[Voltage] {simulatedVoltage:F3} V";
+    }
+}
+
+public class MeasureCurrentStep : TestStep
+{
+    public MeasureCurrentStep() : base("Measure Current") { }
+
+    public override string Execute()
+    {
+        double simulatedCurrent = 0.5 + new Random().NextDouble() * 0.3;
+        return $"[Current] {simulatedCurrent:F3} A";
+    }
+}
---
+++ MVVM_Pruefprogramm/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MVVM_Pruefprogramm.Models;
+
+namespace MVVM_Pruefprogramm.ViewModels;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<TestStep> Steps { get; } = new();
+    public RelayCommand RunStepsCommand { get; }
+    private string _log = "";
+
+    public string Log
+    {
+        get => _log;
+        set { _log = value; OnPropertyChanged(); }
+    }
+
+    public MainViewModel()
+    {
+        Steps.Add(new MeasureVoltageStep());
+        Steps.Add(new MeasureCurrentStep());
+
+        RunStepsCommand = new RelayCommand(ExecuteAllSteps);
+    }
+
+    private void ExecuteAllSteps()
+    {
+        foreach (var step in Steps)
+        {
+            string result = step.Execute();
+            Log += result + Environment.NewLine;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
